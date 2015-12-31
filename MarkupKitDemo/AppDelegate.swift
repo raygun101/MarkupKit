edf9bacc8885af9b14aa527078f9e45579de12d7
Greg Brown <gk_brown@verizon.net>
2015-12-31 16:02:44 -0500

    Minor code style tweaks.

--- MarkupKitDemo/AppDelegate.swift
+++ MarkupKitDemo/AppDelegate.swift
@@ -20,11 +20,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window = UIWindow()
+
+        window!.rootViewController = UINavigationController(rootViewController: ViewController())
+
         window!.backgroundColor = UIColor.whiteColor()
-
-        window!.rootViewController = ViewController()
-
         window!.frame = UIScreen.mainScreen().bounds
+
         window!.makeKeyAndVisible()
 
         return true
