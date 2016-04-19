9ac9e5aebe0465ad58b5ceee389f6b1b6c2676a5
Greg Brown <gk_brown@verizon.net>
2016-04-19 09:46:10 -0400

    Add file templates.

---
+++ Xcode/Templates/MarkupKit/Collection View Cell.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+import MarkupKit
+
+class ___FILEBASENAMEASIDENTIFIER___: LMCollectionViewCell {
+    // TODO: Define outlets for view elements
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        LMViewBuilder.viewWithName("___FILEBASENAMEASIDENTIFIER___", owner: self, root: self)
+    }
+
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder);
+    }
+
+    override func prepareForReuse() {
+        // TODO: Clear contents
+    }
+}
