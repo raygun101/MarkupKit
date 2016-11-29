6a9961f7e63a754d2aaa83e551c66ac5daa9b968
Greg Brown <gk_brown@verizon.net>
2016-11-29 14:31:36 -0500

    Add tvOS target to package script.

--- package.sh
+++ package.sh
@@ -3,6 +3,7 @@ FRAMEWORK=MarkupKit
 BUILD=build
 FRAMEWORK_PATH=$FRAMEWORK.framework
 
+# iOS
 rm -Rf $BUILD
 rm -f $FRAMEWORK_PATH.tar.gz
 
@@ -15,6 +16,20 @@ lipo -create $BUILD/Release-iphoneos/$FRAMEWORK_PATH/$FRAMEWORK $BUILD/Release-i
 
 tar -czv -C $BUILD/Release-universal -f $FRAMEWORK.framework.tar.gz $FRAMEWORK_PATH
 
+#tvOS
+rm -Rf $BUILD.tvOS
+rm -f $FRAMEWORK_PATH.tvOS.tar.gz
+
+xcodebuild archive -project $FRAMEWORK.xcodeproj -scheme $FRAMEWORK.tvOS -sdk appletvos SYMROOT=$BUILD.tvOS
+xcodebuild build -project $FRAMEWORK.xcodeproj -target $FRAMEWORK.tvOS -sdk appletvsimulator SYMROOT=$BUILD.tvOS
+
+cp -RL $BUILD.tvOS/Release-appletvos $BUILD.tvOS/Release-universal
+
+lipo -create $BUILD.tvOS/Release-appletvos/$FRAMEWORK_PATH/$FRAMEWORK $BUILD.tvOS/Release-appletvsimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $BUILD.tvOS/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK
+
+tar -czv -C $BUILD.tvOS/Release-universal -f $FRAMEWORK.framework.tvOS.tar.gz $FRAMEWORK_PATH
+
+# Templates
 rm -f XcodeTemplates.tar.gz
 
 tar -czv -f XcodeTemplates.tar.gz Xcode
