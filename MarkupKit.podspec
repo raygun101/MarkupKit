8b0c04bcfbb6368ee703cbdf3dd6ef831ec87c47
Greg Brown <gk_brown@verizon.net>
2016-12-02 10:31:01 -0500

    Update build script and podspec.

--- MarkupKit.podspec
+++ MarkupKit.podspec
@@ -1,18 +1,14 @@
 Pod::Spec.new do |s|
-  s.name             = 'MarkupKit'
-  s.version          = '2.6.2'
-  s.summary          = 'Declarative UI for iOS applications'
-  s.description      = <<-DESC
-    MarkupKit is an open-source framework for simplifying development of native iOS
-    applications. It allows developers to construct user interfaces declaratively using a
-    human-readable markup language, rather than visually using Interface Builder or
-    programmatically in code.
-    DESC
-  s.homepage         = 'https://github.com/gk-brown/MarkupKit'
-  s.license          = 'Apache License, Version 2.0'
-  s.author           = 'Greg Brown'
-  s.source           = { :git => "https://github.com/gk-brown/MarkupKit.git", :tag => s.version.to_s }
-  s.platform     = :ios, '8.0'
-  s.requires_arc = true
-  s.source_files = 'MarkupKit/*.{h,m}'
+  s.name            = 'MarkupKit'
+  s.version         = '2.7'
+  s.license         = 'Apache License, Version 2.0'
+  s.homepage        = 'https://github.com/gk-brown/MarkupKit'
+  s.author          = 'Greg Brown'
+  s.summary         = 'Declarative UI for iOS applications'
+  s.source          = { :git => "https://github.com/gk-brown/MarkupKit.git", :tag => s.version.to_s }
+
+  s.ios.deployment_target   = '8.0'
+  s.ios.source_files        = 'MarkupKit-iOS/MarkupKit/*.{h,m}'
+  s.tvos.deployment_target  = '10.0'
+  s.tvos.source_files       = 'MarkupKit-iOS/MarkupKit/*.{h,m}'
 end
--- package.sh
+++ package.sh
@@ -4,32 +4,32 @@ BUILD=build
 FRAMEWORK_PATH=$FRAMEWORK.framework
 
 # iOS
-rm -Rf $BUILD
-rm -f $FRAMEWORK_PATH.tar.gz
+rm -Rf $FRAMEWORK-iOS/$BUILD
+rm -f $FRAMEWORK-iOS.framework.tar.gz
 
-xcodebuild archive -project $FRAMEWORK.xcodeproj -scheme $FRAMEWORK -sdk iphoneos SYMROOT=$BUILD
-xcodebuild build -project $FRAMEWORK.xcodeproj -target $FRAMEWORK -sdk iphonesimulator SYMROOT=$BUILD
+xcodebuild archive -project $FRAMEWORK-iOS/$FRAMEWORK-iOS.xcodeproj -scheme $FRAMEWORK -sdk iphoneos SYMROOT=$BUILD
+xcodebuild build -project $FRAMEWORK-iOS/$FRAMEWORK-iOS.xcodeproj -target $FRAMEWORK -sdk iphonesimulator SYMROOT=$BUILD
 
-cp -RL $BUILD/Release-iphoneos $BUILD/Release-universal
+cp -RL $FRAMEWORK-iOS/$BUILD/Release-iphoneos $FRAMEWORK-iOS/$BUILD/Release-universal
 
-lipo -create $BUILD/Release-iphoneos/$FRAMEWORK_PATH/$FRAMEWORK $BUILD/Release-iphonesimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $BUILD/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK
+lipo -create $FRAMEWORK-iOS/$BUILD/Release-iphoneos/$FRAMEWORK_PATH/$FRAMEWORK $FRAMEWORK-iOS/$BUILD/Release-iphonesimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $FRAMEWORK-iOS/$BUILD/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK
 
-tar -czv -C $BUILD/Release-universal -f $FRAMEWORK.framework.tar.gz $FRAMEWORK_PATH
+tar -czv -C $FRAMEWORK-iOS/$BUILD/Release-universal -f $FRAMEWORK-iOS.framework.tar.gz $FRAMEWORK_PATH
 
-#tvOS
-rm -Rf $BUILD.tvOS
-rm -f $FRAMEWORK_PATH.tvOS.tar.gz
+# tvOS
+rm -Rf $FRAMEWORK-tvOS/$BUILD
+rm -f $FRAMEWORK-tvOS.framework.tar.gz
 
-xcodebuild archive -project $FRAMEWORK.xcodeproj -scheme $FRAMEWORK.tvOS -sdk appletvos SYMROOT=$BUILD.tvOS
-xcodebuild build -project $FRAMEWORK.xcodeproj -target $FRAMEWORK.tvOS -sdk appletvsimulator SYMROOT=$BUILD.tvOS
+xcodebuild archive -project $FRAMEWORK-tvOS/$FRAMEWORK-tvOS.xcodeproj -scheme $FRAMEWORK -sdk iphoneos SYMROOT=$BUILD
+xcodebuild build -project $FRAMEWORK-tvOS/$FRAMEWORK-tvOS.xcodeproj -target $FRAMEWORK -sdk iphonesimulator SYMROOT=$BUILD
 
-cp -RL $BUILD.tvOS/Release-appletvos $BUILD.tvOS/Release-universal
+cp -RL $FRAMEWORK-tvOS/$BUILD/Release-iphoneos $FRAMEWORK-tvOS/$BUILD/Release-universal
 
-lipo -create $BUILD.tvOS/Release-appletvos/$FRAMEWORK_PATH/$FRAMEWORK $BUILD.tvOS/Release-appletvsimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $BUILD.tvOS/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK
+lipo -create $FRAMEWORK-tvOS/$BUILD/Release-iphoneos/$FRAMEWORK_PATH/$FRAMEWORK $FRAMEWORK-tvOS/$BUILD/Release-iphonesimulator/$FRAMEWORK_PATH/$FRAMEWORK -output $FRAMEWORK-tvOS/$BUILD/Release-universal/$FRAMEWORK_PATH/$FRAMEWORK
 
-tar -czv -C $BUILD.tvOS/Release-universal -f $FRAMEWORK.framework.tvOS.tar.gz $FRAMEWORK_PATH
+tar -czv -C $FRAMEWORK-tvOS/$BUILD/Release-universal -f $FRAMEWORK-tvOS.framework.tar.gz $FRAMEWORK_PATH
 
 # Templates
-rm -f XcodeTemplates.tar.gz
+# rm -f XcodeTemplates.tar.gz
 
-tar -czv -f XcodeTemplates.tar.gz Xcode
+# tar -czv -f XcodeTemplates.tar.gz Xcode
