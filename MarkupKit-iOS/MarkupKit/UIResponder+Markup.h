11eaaa0cd0583930d58a39878fd31e3790dbc4f6
Greg Brown <gk_brown@icloud.com>
2018-04-24 09:31:38 -0400

    Update source code comments.

--- MarkupKit-iOS/MarkupKit/UIResponder+Markup.h
+++ MarkupKit-iOS/MarkupKit/UIResponder+Markup.h
@@ -39,10 +39,10 @@ NS_ASSUME_NONNULL_BEGIN
 - (nullable NSString *)tableForStrings;
 
 /**
- * Establishes a two-way binding between this object and an associated view instance.
+ * Establishes a binding between this object and a view instance.
  *
  * @param property The key path of a property in this object.
- * @param view The associated view instance.
+ * @param view The view instance.
  * @param keyPath The key path of a property in the view.
  */
 - (void)bind:(NSString *)property toView:(UIView *)view withKeyPath:(NSString *)keyPath;
